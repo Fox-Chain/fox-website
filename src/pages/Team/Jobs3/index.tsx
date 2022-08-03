2d5c816130cb321e2250e4125a5eca74be449764
wangzhe409 <wangzhe18@jd.com>
2022-08-03 21:35:40 +0800

    update

--- src/pages/Team/Jobs3/index.tsx
+++ src/pages/Team/Jobs3/index.tsx
@@ -1,74 +1,83 @@
-import { useLayoutEffect, useState,useRef } from 'react';
+import { useLayoutEffect, useState, useRef } from 'react';
 import { ReactComponent as Arrow } from '@/assets/arrow.svg';
 import { Drawer } from 'antd';
 import styles from './index.less';
 import { connect } from 'umi';
-const JobData = [{
-  job: 'Senior Engineer',
-  desc: `Has at least 5 years of development experience in a high-level object-oriented  language (preference for Python and C++);
-  Is a University graduate in Computer Science (with a high GPA);
-  Has experience building complex, high-scale, high-performance software;
-  Is curious by nature and a quick learner;`
-},
-{
-  job: 'Engineer',
-  desc: `Has at least 1 year of development experience in a high-level object-oriented language (preference for Python and C++);
-  Is a University graduate in Computer Science (with a high GPA);
-  Is curious by nature and a quick learner;`
-},
-{
-  job: 'Marketing Specialist',
-  desc: `An experience in digital and social networks;
-  An experience in versatile content writing;
-  Technologies orientation;
-  Good communication skills (Verbal and written);
-  The ability to make things done (mission-oriented);
-  High interpersonal abilities;
-  Creativity and initiative ;
-  Good learnings abilities;
-  The ability to work Independently;
-  Good Analyzing skills ;
-  English Proficiency;`
-}]
+
 const Banner = (props) => {
   const [jobIndex, setJobindex] = useState(0)
   const [upload1, setUpload1] = useState(false);
   const couterRef = useRef();
-
+  const JobData = [{
+    job: 'Senior Engineer',
+    desc: `5+ years of development experience in a high-level object-oriented language (preference for Rust);
+    University graduate in Computer Science with a high GPA;
+    Experience in building complex, high-scale, high-performance software;
+    Good oral and written English, and technical leadership;`,
+    ref:useRef()
+  },
+  {
+    job: 'Engineer',
+    desc: `2+ years of experience with consensus algorithms;
+    Fluency in Rust, C++, Golang, or similar languages. We are working primarily with Rust, but prior experience with this language is not mandatory;
+    Deep understanding of software engineering best-practices;
+    Good oral and written English;`,
+    ref:useRef()
+  },
+  {
+    job: 'Marketing Specialist',
+    desc: `5+ years of experience in Marketing or related field with an emphasis on product marketing;
+    Experience marketing to developer audiences;
+    Strong interpersonal skills and communication capabilities with the ability to work across both highly technical and highly creative teams;
+    Deep experience and success in managing cross-functional teams to bring products and features to market;`,
+    ref:useRef()
+  }]
   return (<div className={styles.banner}>
-    <div className="Notice Field-note is-visible" data-js="field-note">
-      <span className="Notice-arrow"></span>
-      <span className="Notice-msg"><span style={{color:'#ffffff'}}>We're hiring {JobData[jobIndex].job}</span></span>
-    </div>
-    <div className="info">
-      <p>Requirements:</p>
-      <p>{JobData[jobIndex].desc}</p>
-      <div style={{ textAlign: 'center' }}>
-        <div className='explore-button' onClick={()=>{
-          setUpload1(true);
-          setTimeout(() => {
-          couterRef.current.focus()
-            
-          }, 200);
-          }}>Apply</div>
-      </div>
-      <div onBlur={()=>setUpload1(false)} ref={couterRef} tabIndex="1" className='uploadP' style={{ transform: upload1 && 'translateY(0%)' || 'translateY(100%)' }}>
-            <div style={{ height: '100%', width: '100%' }}>
-              <div className="form-group inputDnD">
-                  <input  type="file" className="form-control-file text-primary font-weight-bold" id="inputFile" accept="image/*" data-title="Drag and drop your resume " />
-                <div className="explore-button" style={{marginBottom:'20px'}}>
-                Submit
+    <div style={{height:'358px',overflow:'hidden'}}>
+      <div style={{transition:'all 0.5s',transform:`translateY(-${jobIndex*357}px)`}}>
+        {
+          JobData.map((item, index) => <div style={{margin:'50px auto'}}>
+            <div className="Notice Field-note is-visible" data-js="field-note">
+              <span className="Notice-arrow"></span>
+              <span className="Notice-msg"><span style={{ color: '#ffffff' }}>We're hiring {JobData[jobIndex].job}</span></span>
+            </div>
+            <div className="info">
+              <p>Requirements:</p>
+              <p>
+                <ul style={{listStyle: 'disc',marginLeft: '14px',    fontSize: '14px',lineHeight: '26px'}}>
+                {JobData[jobIndex].desc.split(";").map((item,index2)=>item!=""&&<li>{item+((index2*1+2)==JobData[jobIndex].desc.split(";").length&&"."||";")}</li>)}
+                  
+                </ul>
+                </p>
+              <div style={{ textAlign: 'center' }}>
+                <div className='explore-button' onClick={() => {
+                  setUpload1(true);
+                  setTimeout(() => {
+                    item.ref.current.focus()
+
+                  }, 200);
+                }}>Apply</div>
+              </div>
+              <div onBlur={() => setUpload1(false)} ref={item.ref} tabIndex={index} className='uploadP' style={{ transform: upload1 && 'translateY(0%)' || 'translateY(100%)' }}>
+              {/* <div  className='uploadP' style={{ transform: upload1 && 'translateY(0%)' || 'translateY(100%)' }}> */}
+                <div style={{ height: '100%', width: '100%' }}>
+                  <div className="form-group inputDnD">
+                    <input type="file" className="form-control-file text-primary font-weight-bold" id="inputFile" accept="image/*" data-title="Drag and drop your resume " />
+                    <div className="explore-button" style={{ marginBottom: '20px' }}>
+                      Submit
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </div>)
+        }
+      </div>
     </div>
     <div className='tabs'>
       {
-        JobData.map(item=><div style={{height:'22px'}}><div className='tabitem'>{item.job}</div></div>)
+        JobData.map((item, index) => <div onClick={() => setJobindex(index)} className='tabitemwrap'><div className='tabitem'><span>{item.job}</span></div></div>)
       }
-      
- 
     </div>
   </div>
   );
